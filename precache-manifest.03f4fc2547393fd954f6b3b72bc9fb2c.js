fcab946315864724ffe55fe83c78cec2e931e87e
Gitravs <kitravee.swk@gmail.com>
2020-03-18 17:48:19 +0700

    Updates

---
+++ precache-manifest.03f4fc2547393fd954f6b3b72bc9fb2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4190c6d47df0ffbe27153dc844c3e48d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4fea98ecacb83b10c808",
+    "url": "/monsters-rolodex/static/css/main.b5278f10.chunk.css"
+  },
+  {
+    "revision": "9618ac727f25fb5f27cc",
+    "url": "/monsters-rolodex/static/js/2.d3b2774f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.d3b2774f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fea98ecacb83b10c808",
+    "url": "/monsters-rolodex/static/js/main.14025774.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
